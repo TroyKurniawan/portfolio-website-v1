b17741c2eb54f1f2be7f030bd8e3efe4628da370
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-03-09 17:29:29 -0800

    Added responsiveness to Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,17 @@
-import bg from '../assets/cloud-bg.jpg';
-// import troy from '../assets/troy-avatar.jpg';
+import troy from '../assets/troy-avatar.jpg';
 import './Hero.css'
 
 export default function Hero() {
   return (
     <div className='bg'>
+      
       <div className='hero-container'>
-        <img src={bg} className='clouds' alt='cloudy background'/>
 
-
-        {/* <video src={bg} autoPlay loop muted playsInline/> */}
-
-
-          {/* <img src={troy} className='troy'/> */}
-          <div className='text-container'>
-              <p className='greeting'>Hello there! My name is</p>
-              <p className='name'>TROY</p>
-          </div>
-
-        {/* <p className='scroll'>SCROLL FOR MORE</p> */}
+        <div>
+          <img src={troy} className='avatar' alt='troy avatar'/>
+          <p className='greeting'>Hello there!<br/> My name is</p>
+          <p className='name'>TROY</p>
+        </div>
 
       </div>
 
