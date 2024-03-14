163135734a4d00e6fbf440a51473bc82a9328085
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-03-14 00:03:33 -0700

    Added responsiveness to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,13 @@ export default function Footer() {
     return (
         <footer>
             <div className='footer-contents'>
-                <div>[LINKS]</div>
-                <div>Built with React, Bootstrap, and love 💞 <br/> Deployed with [ADD SERVICE HERE] 🌐</div>
+
+                <div className='links'>[LINKS]</div>
+
+                <div className='footer-message'>Built with React, Bootstrap, and love 💞 <br/>
+                Deployed with [ADD SERVICE HERE] 🌐<br/><br/>
+                © 2024 Troy Kurniawan</div>
+                
             </div>
         </footer>
     );
--- src/components/masterData.js
+++ src/components/masterData.js
@@ -7,11 +7,11 @@ export const masterData = {
     experience: {
         western_digital: {
             title: 'Western Digital',
-            subtitle: 'DevOps Engineer',
+            subtitle: 'Software Engineer',
             date: '2019 - 2021',
             info: 'Maintained an internal-website hosting webapps used by the department. \
-            Webapps fetched and displayed various user data to coworkers to diagnose user issues, \
-            including device log data (status, errors, updates, capacity, boot up times, etc). \
+            Webapps fetched and displayed various user data to coworkers to diagnose user device issues, \
+            including device log data (status, errors, updates, capacity, boot up times, etc) \
             and user account data (email, login time, device serial numbers, etc). \
             Ensured that coworkers have the tools they need to diagnose and solve the issues of +100,000 device users.'
         }
