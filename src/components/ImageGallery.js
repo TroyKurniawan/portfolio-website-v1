8b480a0cc671adf09552682601ade7fd890b5bd6
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-03-22 21:27:29 -0700

    Added personal images to about section

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,17 +1,20 @@
 import './ImageGallery.css';
+import about1 from '../assets/about1.jpg';
+import about2 from '../assets/about2.jpg';
+import about3 from '../assets/about3.jpg';
 
 export default function ImageGallery() {
   return (
     <>
         <div className='gallery'>
             <a className='img-container'>
-                <img className='blue-shadow-box' alt='img1' src='https://images.unsplash.com/photo-1624633078306-014a4c938ef7'/>
+                <img className='blue-shadow-box' alt='img1' src={about1}/>
             </a>
             <a className='img-container'>
-                <img className='blue-shadow-box' alt='img1' src='https://images.unsplash.com/photo-1470225620780-dba8ba36b745'/>
+                <img className='blue-shadow-box' alt='img1' src={about2}/>
             </a>
             <a className='img-container'>
-                <img className='blue-shadow-box' alt='img1' src='https://images.unsplash.com/photo-1526979118433-63c7344f06f1'/>
+                <img className='blue-shadow-box' alt='img1' src={about3}/>
             </a>
         </div>
     </>
