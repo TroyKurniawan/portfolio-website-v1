c478eaf8d03a646d70b886baafdd540a716b3c22
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-06-01 20:08:34 -0700

    Change to mobile menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,8 +12,8 @@ export default function Nav() {
             {/* Menu */}
             <div className='dd-menu'>
                 <li><a href='#about' className='btn' onClick={buttonCloseMenu}>About</a></li>
-                <li><a href='#experience' className='btn' onClick={buttonCloseMenu}>Experience</a></li>
                 <li><a href='#projects' className='btn' onClick={buttonCloseMenu}>Projects</a></li>
+                <li><a href='#experience' className='btn' onClick={buttonCloseMenu}>Experience</a></li>
                 <li><a href='#education' className='btn' onClick={buttonCloseMenu}>Education</a></li>
                 <li><a href='mailto:troykurniawan97@gmail.com' className='btn' onClick={buttonCloseMenu}>Contact</a></li>
             </div>
